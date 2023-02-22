58ce30c61d314d26adb8f59938b19c2df90f558e
lqh175643 <75191343+lqh175643@users.noreply.github.com>
2023-02-22 07:32:15 +0000

    Deploying to gh-pages from @ lqh175643/bs@1c34e541a4610bb26714ac97d9e351a9c179b48e 🚀

--- assets/order.5842bc47.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index.986d2493.js";function o(r){return e({url:"/order/generate",method:"post",data:r})}function a(r){return e({url:"/order/receive",method:"post",data:r})}function n(r){return e({url:"/order/commit",method:"post",data:r})}export{n as c,o as g,a as r};
